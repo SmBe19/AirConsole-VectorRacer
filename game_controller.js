6f380ad56b4633b342d756f8747178ed2bb1942c
SmBe19 <bsgame27@gmail.com>
2015-12-05 18:54:56 +0100

    implement menus & bombs, add several maps

--- game_controller.js
+++ game_controller.js
@@ -1,9 +1,23 @@
 var airconsole;
 var playerIdEl;
 var analogEl;
+var wonElText;
+var wonEl;
+var mapchooseEl;
+var mapchooseListEl;
 
 var downPos;
 var upPos;
+var touchId;
+
+var isTouchCapable;
+
+var ANALOG_SIZE = 200;
+
+navigator.vibrate = (navigator.vibrate ||
+                     navigator.webkitVibrate ||
+                     navigator.mozVibrate ||
+                     navigator.msVibrate);
 
 function initConsole(){
   airconsole = new AirConsole({"orientation": "landscape"});
@@ -14,7 +28,52 @@ function initConsole(){
     } else {
       playerIdEl.innerHTML = "Game is full";
     }
+  };
+
+  airconsole.onMessage = function(from, data){
+    if(from == AirConsole.SCREEN){
+      console.log(data);
+      if(data.vibrate){
+        navigator.vibrate(data.vibrate);
+      }
+      if(data.action !== undefined){
+        if(data.action == "won"){
+          wonTextEl.innerHTML = "You are on rank " + data.rank + "!";
+          wonEl.hidden = false;
+        } else if(data.action == "chooseMap"){
+          fillMapList(data.maps);
+        } else if(data.action == "mapLoaded"){
+          mapchooseEl.hidden = true;
+        }
+      }
+    }
+  };
+
+  airconsole.onReady = function(){
+    var custom = airconsole.getCustomDeviceState(airconsole.getDeviceId());
+    if(custom !== undefined && custom.choosingMaps !== undefined){
+      fillMapList(custom.choosingMaps);
+    }
+  };
+}
+
+function fillMapList(maps){
+  while(mapchooseListEl.firstChild){
+    mapchooseListEl.removeChild(mapchooseListEl.firstChild);
   }
+  maps.forEach(function(m){
+    var newEl = document.createElement("p");
+    newEl.innerHTML = m;
+    newEl.classList.add("mapchoose_element");
+    newEl.addEventListener("click", function(){
+      airconsole.message(AirConsole.SCREEN, {"action": "loadMap", "mapId": m});
+      mapchooseEl.hidden = true;
+      airconsole.setCustomDeviceStateProperty("choosingMaps", undefined);
+    });
+    mapchooseListEl.appendChild(newEl);
+  });
+  mapchooseEl.hidden = false;
+  airconsole.setCustomDeviceStateProperty("choosingMaps", maps);
 }
 
 function init(){
@@ -22,36 +81,100 @@ function init(){
 
   playerIdEl = document.getElementById("player_id");
   analogEl = document.getElementById("analog");
+  wonTextEl = document.getElementById("wonmessage_text");
+  wonEl = document.getElementById("wonmessage");
+  mapchooseEl = document.getElementById("mapchoose");
+  mapchooseListEl = document.getElementById("mapchoose_list");
+
+  wonEl.hidden = true;
+  mapchooseEl.hidden = true;
 }
 
-function plantBomb(){
+function chooseColor(element, isTouch){
+  if(isTouch){
+    isTouchCapable = true;
+  }
+
+  var bgcolor = getComputedStyle(element, "").backgroundColor;
+  var vgcolor = getComputedStyle(element, "").color;
+
+  airconsole.setCustomDeviceStateProperty("color", bgcolor);
+  document.getElementById("welcome").hidden = true;
+  analogEl.style.background = bgcolor;
+  analogEl.style.color = vgcolor;
+  playerIdEl.style.color = vgcolor;
+  wonEl.style.background = bgcolor;
+  wonTextEl.style.color = vgcolor;
+}
+
+function plantBomb(isTouch){
+  if(isTouchCapable && !isTouch){
+    return;
+  }
   airconsole.message(AirConsole.SCREEN, {"action": "bomb"});
 }
 
-function plantFreezer(){
+function plantFreezer(isTouch){
+  if(isTouchCapable && !isTouch){
+    return;
+  }
   airconsole.message(AirConsole.SCREEN, {"action": "freezer"});
 }
 
-function touchStart(ev){
-  downPos = {"x": ev.clientX, "y": ev.clientY};
+function touchStart(ev, isTouch){
+  if(isTouchCapable && !isTouch){
+    return;
+  }
+  if(ev.touches !== undefined){
+    if(touchId !== undefined || ev.targetTouches.length < 1){
+      return;
+    }
+    var touch = ev.targetTouches.item(0);
+    touchId = touch.identifier;
+    downPos = {"x": touch.screenX, "y": touch.screenY};
+  } else {
+    downPos = {"x": ev.clientX, "y": ev.clientY};
+  }
 }
 
-function touchEnd(ev, isMove){
+function touchMove(ev, isTouch){
+  if(isTouchCapable && !isTouch){
+    return;
+  }
   if(downPos === undefined){
     return;
   }
-  upPos = {"x": ev.clientX, "y": ev.clientY};
+  if(ev.touches !== undefined){
+    upPos = undefined;
+    for(var i = 0; i < ev.touches.length; i++){
+      if(ev.touches.item(i).identifier === touchId){
+        upPos = {"x": ev.touches.item(i).screenX, "y": ev.touches.item(i).screenY};
+      }
+    }
+    if(upPos === undefined){
+      return;
+    }
+  } else {
+    upPos = {"x": ev.clientX, "y": ev.clientY};
+  }
 
   var getDir = function(x, size){
     return Math.abs(x) > size / 6 ? (x < 0 ? -1 : 1) : 0;
   }
 
-  data = {"action": "move", "x": getDir(upPos.x - downPos.x, analogEl.clientWidth),
-    "y": getDir(upPos.y - downPos.y, analogEl.clientHeight)};
+  var data = {"action": "move", "x": getDir(upPos.x - downPos.x, ANALOG_SIZE),
+    "y": getDir(upPos.y - downPos.y, ANALOG_SIZE)};
 
-  if(!isMove){
-    downPos = undefined;
+  airconsole.message(AirConsole.SCREEN, data);
+}
+
+function touchEnd(isTouch){
+  if(isTouchCapable && !isTouch){
+    return;
   }
+  downPos = undefined;
+  touchId = undefined;
 
+  var data = {"action": "move", "x": 0, "y": 0};
   airconsole.message(AirConsole.SCREEN, data);
 }
--- game_screen.js
+++ game_screen.js
@@ -2,32 +2,65 @@ var airconsole;
 var waitEl;
 var canvBg;
 var canvFg
+
+var raceMaps;
+
 var raceMap;
 var raceMapSize;
 var raceMapStart;
+var raceMapCheckpoints;
+
+var game_running;
 var players = [];
+var bombs = [];
 var timeUntilRound;
+var playersInDest;
+var activePlayers;
+
+var mapChooser;
 
 var STRIPES_COUNT = 6;
-var TIME_PER_ROUND = 5000;
+var TIME_PER_ROUND = 2000;
+var START_WAIT_TIME = 10000;
 var ROUND_TIME_UPDATE = 1000;
+var DRAW_TIMEOUT = 100;
+
+var BOMB_RADIUS = 3;
+var FREEZER_RADIUS = 2;
+var FREEZER_DURATION = 2;
 
 function init(){
   waitEl = document.getElementById("wait");
   canvBg = document.getElementById("canvasBg");
   canvFg = document.getElementById("canvasFg");
 
-  loadDefaultMap();
-
   initCanvas(canvBg);
   initCanvas(canvFg);
 
-  drawBackground();
+  raceMaps = initRaceMaps();
+  loadDefaultMap();
 
-  setInterval(drawPlayers, 100);
+  setInterval(drawPlayers, DRAW_TIMEOUT);
   setInterval(maybePlayRound, ROUND_TIME_UPDATE);
 
   initConsole();
+
+  game_running = false;
+
+  // debug
+  //debugInit();
+}
+
+function debugInit(){
+  initPlayer(1);
+  players[1].vx = 1;
+  players[1].vy = 1;
+  players[1].chvx = 1;
+
+  bombs.push({"type": "bomb", "x": 5, "y": 5});
+  bombs.push({"type": "freezer", "x": 15, "y": 5});
+
+  game_running = true;
 }
 
 // *** AirConsole ***
@@ -39,13 +72,15 @@ function initConsole(){
     airconsole.setActivePlayers(playerDeviceIds.length);
     waitEl.innerHTML = "";
     if(players[device_id] === undefined){
-      players[device_id] = {"x": raceMapStart.x, "y": raceMapStart.y, "vx" : 0, "vy" : 0,
-        "chvx": 0, "chvy": 0, "bomb": false, "freezer": false, color: "red", "active": true};
+      initPlayer(device_id);
     } else {
       players[device_id].active = true;
     }
-    console.log(players);
-    drawPlayers();
+    activePlayers++;
+
+    if(playerDeviceIds.length === 1){
+      initMapChoosing();
+    }
   };
 
   airconsole.onDisconnect = function(device_id){
@@ -53,7 +88,13 @@ function initConsole(){
     airconsole.setActivePlayers(playerDeviceIds.length);
     if(playerDeviceIds.length == 0){
       waitEl.innerHTML = "Waiting for players...";
+      game_running = false;
+    } else {
+      if(device_id === mapChooser){
+        initMapChoosing();
+      }
     }
+    activePlayers--;
 
     if(players[device_id] !== undefined){
       players[device_id].active = false;
@@ -70,34 +111,104 @@ function initConsole(){
           plantBomb(device_id);
         } else if(data.action == "freezer"){
           plantFreezer(device_id);
+        } else if(data.action == "loadMap"){
+          loadMap(raceMaps[data.mapId]);
+          mapChooser = false;
         }
       }
     }
+  };
+
+  airconsole.onCustomDeviceStateChange = function(device_id, custom_data){
+    if(players[device_id] !== undefined){
+      players[device_id].color = custom_data.color;
+    }
+  };
+
+  airconsole.onDeviceStateChange = function(device_id, user_data){
   }
+}
+
+function initMapChoosing(){
+  mapChooser = airconsole.convertPlayerNumberToDeviceId(0);
+  if(mapChooser === undefined){
+    mapChooser = false;
+    return;
+  }
+  game_running = false;
+
+  var data = [];
+  for(var k in raceMaps){
+    data.push(k);
+  }
+
+  airconsole.message(mapChooser, {"action": "chooseMap", "maps": data});
 }
 
 // *** Game ***
 function loadDefaultMap(){
-  loadMap("20 20\n#######.$$==.#######\n#####...$$==...#####\n###.....$$==.....###\n##......$$==......##\n##......$##=......##\n#......######......#\n#.....########.....#\n.....##########.....\n.....##########.....\n....############....\n....############....\n.....##########.....\n.....##########.....\n#.....########.....#\n#......######......#\n##......a##a......##\n##......aaaa......##\n###.....aaaa.....###\n#####...aaaa...#####\n#######.aaaa.#######")
+  loadMap(raceMaps["circle"]);
 }
 
 function loadMap(source){
-  raceMap = source.split("\n");
-  raceMapSize = raceMap[0].split(" ");
-  raceMap.shift();
+  if(source.raceMapSize !== undefined){
+    raceMap = source.raceMap;
+    raceMapSize = source.raceMapSize;
+  } else {
+    raceMap = source.split("\n");
+    raceMapSize = raceMap[0].split(" ");
+    raceMap.shift();
+  }
 
   players = [];
-  timeUntilRound = TIME_PER_ROUND;
+  raceMapCheckpoints = [];
+  timeUntilRound = START_WAIT_TIME;
+  playersInDest = 0;
+  activePlayers = 0;
+  var startPoints = [];
+  mapChooser = false;
+  game_running = true;
 
   for(var y = 0; y < raceMapSize[1]; y++){
     for(var x = 0; x < raceMapSize[0]; x++){
       if(raceMap[y][x] == '='){
-        raceMapStart = {"x": x, "y": y};
-        y = raceMapSize[1];
-        break;
+        startPoints.push({"x": x, "y": y});
+      }
+      if(raceMap[y][x] != '#' && raceMap[y][x] != '.'){
+        if(raceMapCheckpoints.indexOf(raceMap[y][x]) === -1){
+          raceMapCheckpoints.push(raceMap[y][x]);
+        }
       }
     }
   }
+
+  raceMapStart = startPoints[Math.floor(startPoints.length / 2)];
+
+  drawBackground();
+
+  initDevicePlayers();
+
+  if(airconsole !== undefined){
+    airconsole.broadcast({"action": "mapLoaded"})
+  }
+}
+
+function initDevicePlayers(){
+  if(airconsole === undefined){
+    return;
+  }
+
+  airconsole.getControllerDeviceIds().forEach(function(p){
+    initPlayer(p);
+    activePlayers++;
+  });
+}
+
+function initPlayer(playerId){
+  players[playerId] = {"x": raceMapStart.x, "y": raceMapStart.y, "vx" : 0, "vy" : 0,
+    "chvx": 0, "chvy": 0, "bomb": false, "freezer": false, color: "red", "active": true,
+    "won": false, "frozen": 0, "playerId": playerId, "rank": 0,
+    "lastCheckpoint": raceMapStart, "visitedCheckpoints": {}};
 }
 
 function move(player, direction){
@@ -120,6 +231,9 @@ function plantFreezer(player){
 }
 
 function maybePlayRound(){
+  if(!game_running){
+    return;
+  }
   timeUntilRound -= ROUND_TIME_UPDATE;
   if(timeUntilRound <= 0){
     timeUntilRound = TIME_PER_ROUND;
@@ -129,18 +243,77 @@ function maybePlayRound(){
 }
 
 function playRound(){
+  bombs = [];
   players.forEach(function(p){
-    if(!p.active){
+    if(!p.active || p.won){
       return;
     }
 
-    p.vx += p.chvx;
-    p.vy += p.chvy;
+    if(p.bomb){
+      bombs.push({"type": "bomb", "x": p.x, "y": p.y});
+      p.bomb = false;
+    }
+    if(p.freezer){
+      bombs.push({"type": "freezer", "x": p.x, "y": p.y});
+      p.freezer = false;
+    }
+
+    if(p.frozen === 0){
+      p.vx += p.chvx;
+      p.vy += p.chvy;
+    } else {
+      p.frozen--;
+    }
     p.x += p.vx;
     p.y += p.vy;
 
-    p.chvx = 0;
-    p.chvy = 0;
+    var c = undefined;
+    if(raceMap[p.y] !== undefined){
+      c = raceMap[p.y][p.x];
+    }
+    if(c === undefined || c == '#'){
+      p.vx = 0;
+      p.vy = 0;
+      p.x = p.lastCheckpoint.x;
+      p.y = p.lastCheckpoint.y;
+    } else if(c != '.'){
+      p.lastCheckpoint = {"x": p.x, "y": p.y};
+      p.visitedCheckpoints[c] = true;
+      if(c == '$'){
+        var isWinner = true;
+        raceMapCheckpoints.forEach(function(e){
+          if(p.visitedCheckpoints[e] !== true){
+            isWinner = false;
+          }
+        });
+        if(isWinner){
+          p.won = true;
+          playersInDest++;
+          p.rank = playersInDest;
+          airconsole.message(p.playerId, {"action": "won", "rank": p.rank});
+
+          if(playersInDest === activePlayers && mapChooser === false){
+            initMapChoosing();
+          }
+        }
+      }
+    }
+  });
+
+  bombs.forEach(function(b){
+    players.forEach(function(p){
+      if((b.type == "bomb" ? BOMB_RADIUS : FREEZER_RADIUS)
+          >= Math.max(Math.abs(p.x-b.x), Math.abs(p.y-b.y))){
+        if(b.type == "bomb"){
+          p.vx = 0;
+          p.vy = 0;
+          airconsole.message(p.playerId, {"vibrate": 1000});
+        } else if(b.type == "freezer"){
+          p.frozen = FREEZER_DURATION;
+          airconsole.message(p.playerId, {"vibrate": 1000});
+        }
+      }
+    });
   });
 }
 
@@ -154,17 +327,35 @@ function initCanvas(canv){
   g.clearRect(0, 0, canv.width, canv.height);
 }
 
-function drawPlayers(){
-  var g = canvFg.getContext("2d");
-  var zoomX = canvFg.clientWidth / raceMapSize[0];
-  var zoomY = canvFg.clientHeight / raceMapSize[1];
+function zoomHelper(canv){
+  var zoomX = canv.clientWidth / raceMapSize[0];
+  var zoomY = canv.clientHeight / raceMapSize[1];
+
+  // make everything square
+  var offX = zoomX > zoomY ? (canv.clientWidth - raceMapSize[0]*zoomY) / 2 : 0;
+  var offY = zoomY > zoomX ? (canv.clientHeight - raceMapSize[1]*zoomX) / 2 : 0;
+  var zoom = Math.min(zoomX, zoomY);
+  zoomX = zoomY = zoom;
 
   var cx = function(x){
-    return (x+0.5)*zoomX;
+    return (x+0.5)*zoomX + offX;
   }
   var cy = function(x){
-    return (x+0.5)*zoomY;
+    return (x+0.5)*zoomY + offY;
   }
+  var dx = function(x){
+    return x*zoomX;
+  }
+  var dy = function(x){
+    return x*zoomY;
+  }
+
+  return {"cx": cx, "cy": cy, "dx": dx, "dy": dy};
+}
+
+function drawPlayers(){
+  var g = canvFg.getContext("2d");
+  var zh = zoomHelper(canvBg);
 
   g.clearRect(0, 0, canvFg.clientWidth, canvFg.clientHeight);
 
@@ -174,60 +365,95 @@ function drawPlayers(){
     }
     g.beginPath();
     g.strokeStyle = p.color;
-    g.arc(cx(p.x), cy(p.y), Math.min(cx(0), cy(0)), 0, 2*Math.PI);
+    // circle
+    g.lineWidth = 2;
+    g.arc(zh.cx(p.x), zh.cy(p.y), Math.min(zh.dx(0.5), zh.dy(0.5)), 0, 2*Math.PI);
     g.stroke();
-    g.moveTo(cx(p.x), cy(p.y));
-    g.lineTo(cx(p.x - p.vx), cy(p.y - p.vy));
+    // old path
+    g.moveTo(zh.cx(p.x), zh.cy(p.y));
+    g.lineTo(zh.cx(p.x - p.vx), zh.cy(p.y - p.vy));
     g.stroke();
-    g.moveTo(cx(p.x), cy(p.y))
-    g.lineTo(cx(p.x + p.vx + p.chvx), cy(p.y + p.vy + p.chvy));
+    // possible positions
+    for(var y = -1; y < 2; y++){
+      for(var x = -1; x < 2; x++){
+        g.beginPath();
+        if((p.frozen === 0 && y == p.chvy && x == p.chvx) ||
+            (p.frozen > 0 && y == 0 && x == 0)){
+          g.strokeStyle = p.color;
+          g.lineWidth = 2;
+        } else {
+          g.strokeStyle = "#CCCCCC";
+          g.lineWidth = 1;
+        }
+        g.arc(zh.cx(p.x + p.vx + x), zh.cy(p.y + p.vy + y), Math.min(zh.dx(0.25), zh.dy(0.25)), 0, 2*Math.PI);
+        g.stroke();
+      }
+    }
+  });
+
+  // bombs
+  g.lineWidth = 2
+  bombs.forEach(function(b){
+    g.beginPath();
+    var radius = 0;
+    if(b.type == "bomb"){
+      g.strokeStyle = "#FF0000";
+      radius = BOMB_RADIUS;
+    } else if(b.type == "freezer"){
+      g.strokeStyle = "#0000FF";
+      radius = FREEZER_RADIUS;
+    }
+    g.moveTo(zh.cx(b.x - radius), zh.cy(b.y));
+    g.lineTo(zh.cx(b.x + radius), zh.cy(b.y));
+    g.stroke();
+    g.moveTo(zh.cx(b.x), zh.cy(b.y-radius));
+    g.lineTo(zh.cx(b.x), zh.cy(b.y+radius));
+    g.stroke();
+    g.moveTo(zh.cx(b.x-radius/2), zh.cy(b.y-radius/2));
+    g.lineTo(zh.cx(b.x+radius/2), zh.cy(b.y+radius/2));
+    g.stroke();
+    g.moveTo(zh.cx(b.x+radius/2), zh.cy(b.y-radius/2));
+    g.lineTo(zh.cx(b.x-radius/2), zh.cy(b.y+radius/2));
     g.stroke();
   });
 }
 
 function drawBackground(){
   var g = canvBg.getContext("2d");
-  var zoomX = canvBg.clientWidth / raceMapSize[0];
-  var zoomY = canvBg.clientHeight / raceMapSize[1];
+  var zh = zoomHelper(canvBg);
 
-  var cx = function(x){
-    return (x+0.5)*zoomX;
-  }
-  var cy = function(x){
-    return (x+0.5)*zoomY;
-  }
+  g.clearRect(0, 0, canvBg.clientWidth, canvBg.clientHeight);
 
-  g.clearRect(cx(-0.5), cy(-0.5), cx(99.5), cy(99.5));
-
+  g.beginPath();
   for(var y = 0; y < raceMapSize[1]; y++){
     for(var x = 0; x < raceMapSize[0]; x++){
       if(raceMap[y][x] != '#'){
         g.fillStyle = "transparent";
         if(raceMap[y][x] == '$'){
-          g.fillStyle = "red";
+          g.fillStyle = "#FAA";
         } else if(raceMap[y][x] == '='){
-          g.fillStyle = "green";
+          g.fillStyle = "#AFA";
         } else if(raceMap[y][x] != '.'){
-          g.fillStyle = "yellow";
+          g.fillStyle = "#FFA";
         }
         if(raceMap[y][x] != '.'){
-          g.fillRect(cx(x-0.5), cy(y-0.5), cx(0.5), cy(0.5));
+          g.fillRect(zh.cx(x-0.5), zh.cy(y-0.5), zh.dx(1), zh.dy(1));
         }
-        g.moveTo(cx(x), cy(y-0.5));
-        g.lineTo(cx(x), cy(y+0.5));
+        g.moveTo(zh.cx(x), zh.cy(y-0.5));
+        g.lineTo(zh.cx(x), zh.cy(y+0.5));
         g.stroke();
-        g.moveTo(cx(x-0.5), cy(y));
-        g.lineTo(cx(x+0.5), cy(y));
+        g.moveTo(zh.cx(x-0.5), zh.cy(y));
+        g.lineTo(zh.cx(x+0.5), zh.cy(y));
         g.stroke();
       } else {
         for(var i = 0; i < STRIPES_COUNT; i++){
           if(i <= STRIPES_COUNT / 2){
-            g.moveTo(cx(x + 0.5 - 2*i/STRIPES_COUNT), cy(y+0.5));
-            g.lineTo(cx(x + 0.5), cy(y + 0.5 - 2*i/STRIPES_COUNT));
+            g.moveTo(zh.cx(x + 0.5 - 2*i/STRIPES_COUNT), zh.cy(y+0.5));
+            g.lineTo(zh.cx(x + 0.5), zh.cy(y + 0.5 - 2*i/STRIPES_COUNT));
             g.stroke();
           } else {
-            g.moveTo(cx(x - 0.5), cy(y + 1.5 - 2*i/STRIPES_COUNT));
-            g.lineTo(cx(x + 1.5 - 2*i/STRIPES_COUNT), cy(y - 0.5));
+            g.moveTo(zh.cx(x - 0.5), zh.cy(y + 1.5 - 2*i/STRIPES_COUNT));
+            g.lineTo(zh.cx(x + 1.5 - 2*i/STRIPES_COUNT), zh.cy(y - 0.5));
             g.stroke();
           }
         }
